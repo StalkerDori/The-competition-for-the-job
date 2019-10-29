b9d7038cd667dc8317f7678131777c8b872d83c9
vasilisa_sisenbaeva <stalker.dori@mail.ru>
2019-10-30 00:41:34 +0500

    SQLite was added by tool NuGet

--- LORENATable/Form1.cs
+++ LORENATable/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace LORENATable
 {
@@ -14,7 +15,13 @@ namespace LORENATable
         public Form1()
         {
             InitializeComponent();
+
+            using (SQLiteConnection connection = new SQLiteConnection())
+            {
+
+            }
         }
+          
 
         private void Button1_Click(object sender, EventArgs e)
         {
